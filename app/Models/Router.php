c16a5aaec60de68e7120ecb9017ea14d266ece7a
Brendown Ferreira <br3n0k@gmail.com>
2024-07-01 16:41:20 -0300

    Att para otimização do retorno da classe ao invez de utilizar classe construtora centralizando todo o load dentro da classe de aplicação do sistema.

--- app/Models/Router.php
+++ app/Models/Router.php
@@ -4,51 +4,53 @@
  * @version   : 1.0.0
  * @author    : Brendown Ferreira
  * @editor    : Brendown Ferreira
- * @updated   : 2024
+ * @updated   : 01/07/2024
  * @*Classe para gerenciar a requisição de rota para o seu respectivo roteador*@
  */
+
 namespace App\Models;
 
 class Router
 {
     public mixed $route;
-    public string $router_file;
     public string $root_path;
+    public string $router_file;
 
-    // Classe construtora
-    public function __construct($route, $root_path)
+    // Classe que vai tratar a requisição na url para um array
+    public function get_route(): array
     {
-        // Verifica se os parametros enviados são validos
-        if(empty($route) OR empty($root_path))
+        // Define um array de retorno vazio para caso de erros
+        $return = [];
+
+        // Verifica se a propriedade de url request recebeu algum conteudo
+        if(empty($this->route))
         {
-            echo "Falha ao iniciar o roteador.";
-            http_response_code(500);
-            exit();
+            // Define a propriedade de url request como vazio
+            $this->route = $return;
+
+            // Retorna o array de retorno vazio
+            return $return;
         }
+        else
+        {
+            // Elimina a barra inicial da requisição crua da URL
+            $this->route = substr($this->route, 1);
 
-        // Define a propriedade root_path da classe
-        $this->root_path = $root_path;
+            // Cria o array de requisição separada por '/'
+            $this->route = explode('/', $this->route);
 
-        // Elimina a barra inicial da requisição crua da URL
-        $route = substr($route, 1);
-
-        // Cria o array de requisição separada por '/'
-        $route = explode('/', $route);
-
-        // Cria o array de requisição para o roteador e armazena na propriedade route da classe
-        $this->route = $route;
-
-        // Chama a função que vai tratar a rota com o roteador
-        $this->call_router();
+            // Retorna o array de requisição
+            return $this->route;
+        }
     }
 
-    // Função que vai tratar a rota com o roteador
-    public function call_router(): void
+    // Função que vai identificar o roteador correspondente a requisição da URL
+    public function get_router(): string
     {
-        // Verifica o indice 0 do array de rota
+        // Verifica o indice 0 do array de rota, a definir o padrão 'home' caso não tenha nenhum valor
         if (empty($this->route[0])) {
             // Define o indice 0 do array de rota como 'home'
-            $this->route[0] = 'home';
+            $this->route[0] = 'web';
         }
 
         // Verifica se o arquivo do roteador existe
@@ -62,7 +64,7 @@ class Router
             $this->router_file = $this->root_path . 'app/Routes/web.php';
         }
 
-        // Chama o roteador
-        include($this->router_file);
+        // Retorna o caminho do arquivo do roteador
+        return $this->router_file;
     }
 }
