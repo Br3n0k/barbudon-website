70812edd0dea77d7492053b319be3d87d82a5527
Brendown Ferreira <br3n0k@gmail.com>
2024-07-01 16:42:01 -0300

    Implementação do header do html com uma 'part'

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -1,20 +1,8 @@
 <html data-bs-theme="dark" lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home - <?=$_ENV['APP_NAME']?></title>
-    <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="theme-color" content="#000000">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link href="<?=$_ENV['APP_NAME'].'css/app.css'?>" rel="stylesheet">
-    <link href="<?=$_ENV['APP_NAME'].'css/home.css'?>" rel="stylesheet">
-    <?PHP
-        // Inclui as partes de SEO do sistema
-        include_once ($this->root_path.'resources/views/parts/seo.php');
-    ?>
-</head>
+<?PHP
+    // Inclui o Header do HMTL
+    include_once ($this->root_path.'resources/views/parts/header.php');
+?>
 <body>
     <div class="container-fluid p-0">
         <?PHP
