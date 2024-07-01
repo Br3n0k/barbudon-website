43d3c4def23a477559e439ef6ebdf6a84fdf47db
Brendown Ferreira <br3n0k@gmail.com>
2024-07-01 15:43:34 -0300

    Separação do SEO em um unico arquivo.

---
+++ resources/views/parts/seo.php
@@ -0,0 +1,14 @@
+<meta name="twitter:title" content="<?=$_ENV['APP_NAME'].' - '.$_ENV['APP_DESCRIPTION']?>" />
+<meta name="twitter:description" content="<?=$_ENV['APP_SEO_DESCRIPTION']?>" />
+<meta property="og:image" content="1024x1024.png" />
+<meta name="description" content="<?=$_ENV['APP_SEO_DESCRIPTION']?>" />
+<meta name="twitter:image" content="1024x1024.png" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="og:title" content="<?=$_ENV['APP_NAME'].' - '.$_ENV['APP_DESCRIPTION']?>" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?=$_ENV['APP_URL'].'img/icons/apple-touch-icon.png'?>"">
+<link rel="icon" type="image/png" sizes="32x32" href="<?=$_ENV['APP_URL'].'img/icons/favicon-32x32.png'?>">
+<link rel="icon" type="image/png" sizes="16x16" href="<?=$_ENV['APP_URL'].'img/icons/favicon-16x16.png'?>">
+<link rel="manifest" href="<?=$_ENV['APP_URL'].'img/icons/site.webmanifest'?>">
+<link rel="mask-icon" href="<?=$_ENV['APP_URL'].'img/icons/safari-pinned-tab.svg'?>" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="<?=$_ENV['APP_NAME']?>">
+<meta name="application-name" content="<?=$_ENV['APP_NAME']?>">
