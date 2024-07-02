f646779c8d7441d66d9e3d20f984048287e07ffd
Brendown Ferreira <br3n0k@gmail.com>
2024-07-02 12:08:18 -0300

    Implementação do Controller da API

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -8,16 +8,8 @@
  * Arquivo de roteamento para requisições de navegação da parte de api do frontend
  */
 
-// Define o retorno das informações como JSON
-header("Content-Type: application/json; charset=utf-8");
+// Define o caminho para o controlador
+$this->controller = $this->root_path . 'app/Controllers/Api.php';
 
-
-// Define um retorno padrão da api em caso de falhas
-$this->api_response = [
-    'success' => false,
-    'code' => 500,
-    'message' => 'Falha na inicialização da API',
-];
-
-http_response_code($this->api_response['code']);
-echo json_encode($this->api_response, JSON_UNESCAPED_UNICODE);
+// Inclui o controlador
+include_once($this->controller);
